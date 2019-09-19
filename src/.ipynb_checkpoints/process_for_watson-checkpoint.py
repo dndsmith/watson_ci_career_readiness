b59138d04ff1dda5c9a3553b14df60497a4007fc
Daniel Smith <dnls1297@gmail.com>
2019-09-19 13:18:19 -0400

    make data sets for calibration sets with Manu and Daphne

--- src/.ipynb_checkpoints/process_for_watson-checkpoint.py
+++ src/.ipynb_checkpoints/process_for_watson-checkpoint.py
@@ -1,23 +1,27 @@
 # version 1: created watson.csv from competencies.csv
 # version 2: created watson.csv from set1.csv
 # NOTE: README -> doesn't account for entries without proper quotation marks surrounding the text yet
+# version 3: changing this code to be more generic NOTE DONE YET
+
+import sys
+
 if __name__ == "__main__":
+    in_file = open(sys.argv[1], "r")
     out_file = open("../data/watson.csv", "w")
     first = 0
-    with open("../data/set1.csv", "r") as in_file:
-        for line in in_file:
-            if first == 0:
-                first += 1
-                continue
-            sep_by_commas = line.split(",")
-            text = ""
-            length = len(sep_by_commas)
-            for i in range(length-3):
-                text += sep_by_commas[i] + ","
-                print(sep_by_commas[i])
-            if sep_by_commas[length-3] == "1":
-                lead_style = "transformational\n"
-            else:
-                lead_style = "transactional\n" if (sep_by_commas[length-2] == "1") else "unknown\n"
-            response_full = text + lead_style
-            out_file.write(response_full)
+    for line in in_file:
+        if first == 0:
+            first += 1
+            continue
+        sep_by_commas = line.split("|")
+        text = ""
+        length = len(sep_by_commas)
+        for i in range(length-3):
+            text += sep_by_commas[i] + ","
+            print(sep_by_commas[i])
+        if sep_by_commas[length-3] == "1":
+            lead_style = "transformational\n"
+        else:
+            lead_style = "transactional\n" if (sep_by_commas[length-2] == "1") else "unknown\n"
+        response_full = text + lead_style
+        out_file.write(response_full)
